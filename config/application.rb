6e018980f06f2e93314c6df60284d15774cadcdf
Robert Novak <me@robert-novak.com>
2017-04-30 13:39:43 -0700

    setting up devise more, enabling registration via email and logging in

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,17 @@ module Tmp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+        :address => 'smtp.gmail.com',
+        :port => 465,
+        :user_name => ENV['RAILS_SMTP_USERNAME'],
+        :password => ENV['RAILS_SMTP_PASSWORD'],
+        :authentication => :plain,
+        :ssl => true
+    }
+
   end
 end
