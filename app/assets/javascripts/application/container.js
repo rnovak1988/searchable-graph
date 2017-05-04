b44c93294361d1a176419fa2b87e5452b9ae4921
Robert Novak <me@robert-novak.com>
2017-05-04 15:14:17 -0700

    make more modular and jazz

--- app/assets/javascripts/application/container.js
+++ app/assets/javascripts/application/container.js
@@ -10,7 +10,37 @@
 
 (function() {
 
-    var container = angular.module('graph.main', ['ngResource', 'ngRoute']);
+    /**
+     * The container is the root of the application, and handles event propogation
+     * @type {angular.Module}
+     */
+    var container = angular.module('graph.container', ['ngRoute']);
+
+    var rootController = function($rootScope, $scope, $window) {
+
+        $scope.window = $window;
+
+        $scope.application_state = $window.APPLICATION_SATES.HOME;
+        $scope.document_state = null;
+        $scope.graph_state = null;
+
+        $rootScope.current_document = null;
+        $rootScope.current_graph = null;
+
+    };
+
+    var routeConfig = function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/templates/home.html'
+            })
+            .when('/document/', {
+                templateUrl: '/templates/document.html'
+            });
+    };
+
+    container.controller('rootController', ['$rootScope', '$scope', '$window', rootController]);
+    container.config(['$routeProvider', routeConfig]);
 
 
 })();
